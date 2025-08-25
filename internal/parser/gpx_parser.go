71eec57ec018b683dca19e8def4644d712a4f09e
sstent <stuart.stent@gmail.com>
2025-08-25 05:26:16 -0700

    checkpoint 3

--- internal/parser/gpx_parser.go
+++ internal/parser/gpx_parser.go
@@ -5,7 +5,7 @@ import (
 	"math"
 	"time"
 
-	"github.com/sstent/garminsync-go/internal/parser/activity/activity"
+	"github.com/sstent/garminsync-go/internal/parser/activity/activity/activity/activity"
 )
 
 // GPX represents the root element of a GPX file
